31cb880b5886ad8f847541e2b8a54a25b7e9c942
gnahzil <gnahzil050@163.com>
2016-11-10 13:51:24 +0800

    4th issue

--- Algorithm/src/com/ibm/ram/ary/DogCatQueue.java
+++ Algorithm/src/com/ibm/ram/ary/DogCatQueue.java
@@ -51,12 +51,67 @@ public class DogCatQueue {
 	
 	private Queue<PetEnterQueue> dogQ;
 	private Queue<PetEnterQueue> catQ;
+	private int count;
 	
 	public DogCatQueue() {
-		dogQ = new LinkedList<PetEnterQueue>();
-		catQ = new LinkedList<PetEnterQueue>();
+		this.dogQ = new LinkedList<PetEnterQueue>();
+		this.catQ = new LinkedList<PetEnterQueue>();
+		this.count = 0;
 	}
-
+	
+	public void add(Pet pet) {
+		if ("dog".equals(pet.getPetType())) {
+			dogQ.add(new PetEnterQueue(pet, count++));
+		} else if ("cat".equals(pet.getPetType())) {
+			catQ.add(new PetEnterQueue(pet, count++));
+		} else {
+			throw new RuntimeException("Your object is not dog or cat");
+		}
+	}
+	
+	public Pet pollAll() {
+		if (!dogQ.isEmpty() && !catQ.isEmpty()) {
+			if (dogQ.peek().getCount() < catQ.peek().getCount()) {
+				return dogQ.poll().getPet();
+			} else {
+				return catQ.poll().getPet();
+			}
+		} else if (!dogQ.isEmpty()) {
+			return dogQ.poll().getPet();
+		} else if (!catQ.isEmpty()) {
+			return catQ.poll().getPet();
+		} else {
+			throw new RuntimeException("queue is empty");
+		}
+	}
+	
+	public Dog pollDog() {
+		if (!dogQ.isEmpty()) {
+			return (Dog)dogQ.poll().getPet();
+		} else {
+			throw new RuntimeException("dog queue is empty");
+		}
+	}
+	
+	public Cat pollCat() {
+		if (!catQ.isEmpty()) {
+			return (Cat)catQ.poll().getPet();
+		} else {
+			throw new RuntimeException("cat queue is empty");
+		}
+	}
+	
+	public boolean isEmpty() {
+		return dogQ.isEmpty() && catQ.isEmpty();
+	}
+	
+	public boolean isDogQueueEmpty() {
+		return dogQ.isEmpty();
+	}
+	
+	public boolean isCatQueueEmpty() {
+		return catQ.isEmpty();
+	}
 }
 
 
